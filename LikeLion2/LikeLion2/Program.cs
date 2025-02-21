a69ebc2e64535a720a9b489bb25f2fe47767540e
정영호 <gggrhrh98@naver.com>
2025-02-21 10:23:35 +0900

    study2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age;    //정수형 변수
+            age = 25;   //변수에 값 저장
+
+            Console.WriteLine(age); //엔터가 포함된 writeLine 엔터가 없는 write
+        }
+    }
+}
