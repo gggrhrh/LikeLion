a8813d843950cae9bc542f19ffd70299f1775b54
정영호 <gggrhrh98@naver.com>
2025-02-21 11:00:58 +0900

    study3

---
+++ study3/study3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            double attack = 56.7;
+            string name = "자크";
+            char grade = 'S';
+
+            Console.WriteLine("캐릭터정보");
+            Console.WriteLine($"hp : {hp}");
+            Console.WriteLine($"att : {attack}");
+            Console.WriteLine($"캐릭터이름 : {name}");
+            Console.WriteLine($"등급 : {grade}");
+
+        }
+    }
+}
