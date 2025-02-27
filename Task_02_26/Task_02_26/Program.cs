e250d7ea80398895018abc687b0c07c8f539f195
정영호 <gggrhrh98@naver.com>
2025-02-27 10:14:46 +0900

    study13

---
+++ Task_02_26/Task_02_26/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("------ 1번 문제 ------");
+            //int[] numbers = { 10, 20, 30, 40, 50 };
+
+            //for(int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.Write(numbers[i] + " ");
+            //}
+
+            //Console.WriteLine("------ 2번 문제 ------");
+            //int[] numbers = new int[5];
+            //int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.Write("정수를 입력하세요 : ");
+            //    numbers[i] = int.Parse(Console.ReadLine());
+            //    sum += numbers[i];
+            //}
+            //Console.WriteLine($"정수의 총합 : {sum}");
+
+
+            //Console.WriteLine("------ 3번 문제 ------");
+            //int[] numbers = { 3, 8, 15, 6, 2 };
+            //int maxNum = 0;
+
+            //for(int i= 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] > maxNum)
+            //    {
+            //        maxNum = numbers[i];
+            //    }
+            //}
+            //Console.WriteLine($"최댓값 : {maxNum}");
+
+
+            //Console.WriteLine("------ 4번 문제 ------");
+
+            //for (int i = 1; i < 11; i++)
+            //    Console.Write(i + " ");
+
+
+            //Console.WriteLine("------ 5번 문제 ------");
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //    i++;
+            //}
+
+
+            //Console.WriteLine("------ 6번 문제 ------");
+            //int[] nums = { 1, 2, 3, 4, 5 };
+
+            //foreach (int i in nums)
+            //    Console.Write(i + " ");
+
+
+            //Console.WriteLine("------ 7번 문제 ------");
+
+            //Console.Write("첫 번째 정수를 입력하세요 : ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("두 번째 정수를 입력하세요 : ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"{a} + {b} = {Sum(a, b)}");
+
+
+            //Console.WriteLine("------ 8번 문제 ------");
+
+            //Console.Write("문자열을 입력하세요 : ");
+            //string input = Console.ReadLine();
+            //Console.WriteLine(Length(input));
+
+
+            Console.WriteLine("------ 9번 문제 ------");
+
+            Console.Write("첫 번째 정수를 입력하세요 : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("두 번째 정수를 입력하세요 : ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("세 번째 정수를 입력하세요 : ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a}, {b}, {c}중 가장 큰 수는 {FindMaxNum(a, b, c)}입니다.");
+
+
+        }//static void Main(string[] args)
+
+        static int FindMaxNum(int a, int b, int c)
+        {
+            int maxnum = 0;
+
+            if (a > maxnum)
+                maxnum = a;
+            if (b > maxnum)
+                maxnum = b;
+            if (c > maxnum)
+                maxnum = c;
+
+            return maxnum;
+        }
+
+        static int Length(string str)
+        {
+            return str.Length;
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ study13/study13/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study13
+{
+    class Program
+    {
+
+        static void PrintHello()
+        {
+            Console.WriteLine("안녕하세요.");
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static int GetNumber()
+        {
+            return 42;
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        //기본값을 가진 매개변수
+        static void Greet(string name = "손님")
+        {
+            Console.WriteLine($"안녕하세요 , {name}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Greet();
+            //Greet("철수");
+
+
+            //PrintHello();
+            //PrintMessage("반갑습니다.");
+
+            //int num = GetNumber();
+            //Console.WriteLine(num);
+
+            //int result = Add(3, 5);
+            //Console.WriteLine(result);
+
+
+            //Console.WriteLine(Multiply(3,4));
+            //Console.WriteLine(Multiply(1.3, 3.4));
+
+            //int q, r;
+            //Divide(10, 3, out q, out r);
+
+            //Console.WriteLine($"몫 : {q}, 나머지 : {r}");
+
+            int value = 5;
+            Increase(ref value);
+
+            Console.WriteLine(value);
+
+        }
+
+        //ref 키워드(값을 참조하여 수정)
+        static void Increase(ref int num)
+        {
+            num += 10;
+        }
+
+
+
+        //out 키워드   (여러 값을 반환할때)
+        static void Divide(int a, int b, out int quotient, out int remainder)
+        {
+            quotient = a / b;
+
+            remainder = a % b;
+        }
+
+
+
+
+        //함수 오버로딩(Overloading)  타입이 다른 변수도 사용하여 같은 이름의 함수를 만들수있음
+
+        /// <summary>
+        /// 두수를 곱하는 함수
+        /// </summary>
+        /// <param name="a">int, double 오버로딩해놈</param>
+        /// <param name="b">int, double 오버로딩해놈</param>
+        /// <returns></returns>
+        static int Numtiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+
+    }
+}
