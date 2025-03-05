76f705fbcd93ecc6d8e0aa151fb6077dc78561ef
정영호 <gggrhrh98@naver.com>
2025-03-05 17:02:57 +0900

    study32

---
+++ Task_03_05LOL/Task_03_05LOL/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03_05LOL
+{
+
+    public class Champion
+    {
+        public string Name = "";
+        public int HP = 0;
+        public int MP = 0;
+        public int Attack = 0;
+        public string ChampionSkill = "";
+
+
+        public Champion()
+        {
+            Name = "갈리오";
+            HP = 600;
+            MP = 410;
+            Attack = 59;
+            ChampionSkill = "전장의 돌풍";
+
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"현재상태 | 이름 : {Name}  체력 : {HP}  마나 : {MP}  공격력 : {Attack}");
+        }
+
+        public virtual void UseSkill(Champion champion)
+        {
+            Console.WriteLine($"{champion.Name}에게 {ChampionSkill}을 사용합니다. ");
+        }
+
+
+    }
+
+    class Blitzcrank : Champion
+    {
+        public Blitzcrank()
+        {
+            Name = "블리츠크랭크";
+            HP = 600;
+            MP = 267;
+            Attack = 60;
+            ChampionSkill = "로켓 손";
+
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+        }
+
+        public override void UseSkill(Champion champion)
+        {
+            Console.WriteLine($"{ChampionSkill}을 사용합니다.{champion.Name}을 끌어옵니다.");
+        }
+
+    }
+
+    class Jayce : Champion
+    {
+        public Jayce()
+        {
+            Name = "제이스";
+            HP = 590;
+            MP = 375;
+            Attack = 59;
+            ChampionSkill = "전격 폭발";
+        }
+
+        public override void UseSkill(Champion champion)
+        {
+            base.UseSkill(champion);
+        }
+
+    }
+
+    class Sylas : Champion
+    {
+        public Sylas()
+        {
+            Name = "사일러스";
+            HP = 600;
+            MP = 400;
+            Attack = 61;
+            ChampionSkill = "국왕시해자";
+        }
+        public override void UseSkill(Champion champion)
+        {
+            Console.WriteLine($"{ChampionSkill}을 사용해 체력을 회복합니다.");
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Champion> champions = new List<Champion>();
+            champions.Add(new Blitzcrank());
+            champions.Add(new Jayce());
+            champions.Add(new Champion());
+            champions.Add(new Sylas());
+
+            Random rand = new Random();
+            foreach (var champ in champions)
+            {
+                champ.ShowInfo();
+                champ.UseSkill(champions[rand.Next(0, 4)]);
+            }
+
+        }
+    }
+}
---
+++ study33/study33/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study33
+{
+
+    class Unit
+    {
+        public string Name;
+        protected int Health;
+
+        public Unit()
+        {
+            Name = "Unknown";
+            Health = 0;
+        }
+
+        public virtual void Attack()
+        {
+            Console.WriteLine($"{Name}이 기본 공격을 합니다.");
+        }
+
+        public virtual void Heal(Unit target)
+        {
+            Console.WriteLine($"{Name}은 치료할 수 없습니다.");
+        }
+
+        public virtual void Move()
+        {
+            Console.WriteLine($"{Name}이 이동합니다.");
+        }
+
+
+
+    }
+
+    class SCV : Unit
+    { 
+        public SCV()
+        {
+            Name = "SCV";
+            Health = 60;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("SCV가 용접기로 공격합니다.! (공격력이 약함)");
+        }
+
+        public override void Heal(Unit target)
+        {
+            Console.WriteLine($"SCV가 {target.Name}을 수리합니다. (기계유닛만 가능)");
+        }
+
+
+    }
+
+    class Marine : Unit
+    {
+        public Marine()
+        {
+            Name = "Marine";
+            Health = 40;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("Marine이 소총으로 공격합니다.");
+        }
+
+
+
+    }
+
+    class Medic : Unit
+    {
+        public Medic()
+        {
+            Name = "Medic";
+            Health = 50;
+        }
+
+        public override void Heal(Unit target)
+        {
+            Console.WriteLine($"Medic이 {target.Name}을 치료합니다. (생명유닛만 가능)");
+        }
+
+
+    }
+
+    class Tank : Unit
+    {
+        public Tank()
+        {
+            Name = "Tank";
+            Health = 150;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("Tank가 시즈 모드로 강력한 포격!");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("탱크가 천천히 움직입니다.");
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Unit> units = new List<Unit>();
+
+            units.Add(new SCV());
+            units.Add(new Marine());
+            units.Add(new Medic());
+            units.Add(new Tank());
+
+            foreach(var i in units)
+            {
+                i.Move();
+                i.Attack();
+                Console.WriteLine();
+            }
+
+            SCV scv = new SCV();
+            scv.Heal(units[3]);
+
+            Medic medic = new Medic();
+
+            medic.Heal(units[1]);
+
+
+        }
+    }
+}
