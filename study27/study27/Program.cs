e9d0f54338070f4ba542c1c70e230cab95cfefc1
정영호 <gggrhrh98@naver.com>
2025-03-05 13:14:37 +0900

    study28

---
+++ study27/study27/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study27
+{
+
+    class Animal
+    {
+        public string Name { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{Name}이(가) 음식을 먹고 있습니다.");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine($"{Name}이(가) 멍멍 짖습니다.");
+        }
+    }
+
+    class Player
+    {
+        public string name;
+
+        public void Render()
+        {
+            Console.WriteLine("플레이어 " + name);
+        }
+    }
+
+    class Wizard : Player
+    {
+        public string job;
+
+        public void Render2()
+        {
+            Console.WriteLine("직업은 " + job + "입니다.");
+        }
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p = new Player();
+
+            p.name = "홍길동";
+            p.Render();
+
+            Wizard w = new Wizard();
+            w.name = "대마법사";
+            w.job = "마법사";
+            w.Render();
+            w.Render2();
+
+        }
+    }
+}
---
+++ study28/study28/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study28
+{
+
+    //메서드 오버라이딩(Overriding)
+    //부모 클래스의 메서드를 자식 클래스에서 재 정의(override)하여 다른 동작을 수행할수있습니다
+
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("동물이 소리를 냅니다.");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name}이(가) 멍멍 짖습니다.");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            myAnimal.Name = "일반동물";
+            myAnimal.Speak();
+
+            Dog myDog = new Dog();
+            myDog.Name = "바둑이";
+            myDog.Speak();
+
+
+        }
+    }
+}
